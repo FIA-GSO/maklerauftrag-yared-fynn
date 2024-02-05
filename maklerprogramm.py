cf456d286d7074c8e5fe28690f870f4ad45e6736
fynn.batje <fybae@gmx.de>
2024-02-05 13:51:33 +0100

    inline Kommentare vorbereitet

---
+++ maklerprogramm.py
@@ -0,0 +1,12 @@
+print("Wie viele Räume gibt es?")
+room_count = input()
+print(room_count)
+# Raumanzahl merken
+# Raumbezeichnungen eingeben
+# Unberechnete Räume vorhanden ?
+# Wenn wahr gebe Seitenlänge ein
+# Wenn nein Ende
+# Wie viele Rechtecke sind vorhanden
+# Für jedes Rechteck Fläche berechnen
+# Ausgabe der Raumflächen mit Raumbezeichnungen
+# Ausgabe der Gesamtfläche
