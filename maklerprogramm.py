4140ec2018b186ae400e23ed427c16c143d5acdd
fynn.batje <fybae@gmx.de>
2024-02-19 15:10:40 +0100

    initial Commit des Maklerprogramms

--- maklerprogramm.py
+++ maklerprogramm.py
@@ -1,42 +1,40 @@
 room_dictionary = {
- "Id": [],
- "Name": [],
- "Area": []
+    "Id": [],
+    "Name": [],
+    "Area": []
 }
 
 has_another_room = True
 counter = 1
 
-while (has_another_room == True):
- room_dictionary["Id"].append(counter)
- print("Bitte gib den Raumbezeichnung an")
- room_name = input()
- has_another_area = True
- room_area = 0
- while (has_another_area == True):
-  room_dictionary["Name"].append(room_name)
-  print("Bitte gib die Länge an")
-  room_length = int(input())
-  print("Bitte gib die Breite an")
-  room_width = int(input())
-  room_area += room_width * room_length
-  print("Gibt es noch eine weiter Fläche [Y/N]")
-  if (input() == "N"):
-    has_another_area = False
+while has_another_room:
+    room_dictionary["Id"].append(counter)
+    room_name = input("Bitte gib den Raumbezeichnung an")
+    room_dictionary["Name"].append(room_name)
+    has_another_area = True
+    room_area = 0
+    while has_another_area:
+        room_length = int(input("Bitte gib die Länge an"))
+        room_width = int(input("Bitte gib die Breite an"))
+        room_area += room_width * room_length
+        next_area = input("Gibt es noch ein weiteres Rechteck? [Y/N]").strip()
+        while next_area != "N" and next_area != "n" and next_area != "Y" and next_area != "y":
+            print("Falsche Eingabe -> [Y/N]")
+            next_area = input().strip()
+        if next_area == "N" or next_area == "n":
+            has_another_area = False
+            room_dictionary["Area"].append(room_area)
+            break
+        elif next_area == "Y" or next_area == "y":
+            continue
     room_dictionary["Area"].append(room_area)
-
-
- room_dictionary["Area"].append(room_area)
- # Gesamtfläche
- # Raumbezeichnungen eingeben
- # Unberechnete Räume vorhanden ?
- # Wenn wahr gebe Seitenlänge ein
- # Noch eine Fläche ?
- # Wenn ja
- # Wenn nein Ende
- # Addiere auf Gesamtfläche
- # Noch ein Raum ?
- # Wenn ja Zeile 7
-# Wenn nein Ende
-# Ausgabe der Raumflächen mit Raumbezeichnungen
-# Ausgabe der Gesamtfläche
+    next_room = input("Gibt es noch einen weiteren Raum? [Y/N]")
+    while next_room != "N" and next_room != "n" and next_room != "Y" and next_room != "y":
+        print("Falsche Eingabe -> [Y/N]")
+    if next_room == "N" or next_room == "n":
+        has_another_room = False
+        print(room_dictionary["id"][1])
+        print("Gesamtflaeche: ")
+        break
+    elif next_room == "Y" or next_room == "y":
+        continue
